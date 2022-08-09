1c9fb90716198359a9a178e7ef22254f06a4a95f
hmatsu47 <35142774+hmatsu47@users.noreply.github.com>
2022-08-09 12:55:20 +0900

    Update test codes

--- JiroApi/src/test/java/com/example/model/OrderTest.java
+++ JiroApi/src/test/java/com/example/model/OrderTest.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("注文内容のバリデーションが想定通り成功／失敗すること")
 class OrderTest {
 
 	@ParameterizedTest
-	@DisplayName("01.正常パターンのバリデーション成功")
+	@DisplayName("正しい注文内容のバリデーションが成功すること")
 	@CsvSource({
 	    "ラーメン, なし, 指定なし, 抜き, 少なめ, マシ",
 	    "ぶたラーメン, 少なめ, 抜き, 少なめ, マシ, マシマシ",
@@ -25,30 +26,54 @@ class OrderTest {
 	}
 
 	@ParameterizedTest
-	@DisplayName("02.異常パターンのバリデーション失敗")
+	@DisplayName("不正なチケットのバリデーションが失敗すること")
 	@CsvSource({
-	    "中ラーメン, なし, 指定なし, 抜き, 少なめ, マシ",
-	    "ラーメン, あり, 抜き, 少なめ, マシ, マシマシ",
-	    "ぶたラーメン, 少なめ, 山盛り, マシ, マシマシ, 指定なし",
-	    "ぶたダブルラーメン, 半分, マシ, 3粒, 指定なし, 抜き",
-	    "ラーメン, 1/3, マシマシ, 指定なし, ゼロ, 少なめ",
-	    "大ラーメン, なし, 指定なし, 抜き, 少なめ, 本気",
-	    "null, なし, 指定なし, 抜き, 少なめ, マシ",
-	    "ラーメン, null, 抜き, 少なめ, マシ, マシマシ",
-	    "ぶた入り大ラーメン, なし, null, マシ, マシマシ, 指定なし",
-	    "ぶたダブル大ラーメン, なし, マシ, null, 指定なし, 抜き",
-	    "ラーメン, 1/3, マシマシ, 指定なし, null, 少なめ",
-	    "大ラーメン, なし, 指定なし, 抜き, 少なめ, null",
-	    "ぶた入り大ラーメン, 少なめ, 抜き, 少なめ, マシ, マシマシ"
+	    "タンメン, なし, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "冷やし中華, なし, 指定なし, 指定なし, 指定なし, 指定なし"
+	})
+	void ticketValidation(String ticketLabel, String lotOption, String yasai, String ninniku, String abura, String karame) {
+		Order order = new Order(ticketLabel, lotOption, yasai, ninniku, abura, karame);
+		assertThrows(RuntimeException.class, () -> order.validate());
+	}
+
+	@ParameterizedTest
+	@DisplayName("不正なロットオプションのバリデーションが失敗すること")
+	@CsvSource({
+	    "ラーメン, あり, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "大ラーメン, 少なめ, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "ぶた入り大ラーメン, 半分, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "ぶたダブル大ラーメン, 1/3, 指定なし, 指定なし, 指定なし, 指定なし"
+	})
+	void lotOptionValidation(String ticketLabel, String lotOption, String yasai, String ninniku, String abura, String karame) {
+		Order order = new Order(ticketLabel, lotOption, yasai, ninniku, abura, karame);
+		assertThrows(RuntimeException.class, () -> order.validate());
+	}
+	
+	@ParameterizedTest
+	@DisplayName("不正なコールのバリデーションが失敗すること")
+	@CsvSource({
+	    "ラーメン, なし, 山盛り, 指定なし, 指定なし, 指定なし",
+	    "ラーメン, なし, 指定なし, 3粒, 指定なし, 指定なし",
+	    "ラーメン, なし, 指定なし, 指定なし, ベトベト, 指定なし",
+	    "ラーメン, なし, 指定なし, 指定なし, 指定なし, 濃いめ"
 	})
 	void failValidation(String ticketLabel, String lotOption, String yasai, String ninniku, String abura, String karame) {
-		Order order = new Order(
-				(ticketLabel.equals("null") ? null : ticketLabel), 
-				(lotOption.equals("null") ? null : lotOption), 
-				(yasai.equals("null") ? null : yasai), 
-				(ninniku.equals("null") ? null : ninniku), 
-				(abura.equals("null") ? null : abura), 
-				(karame.equals("null") ? null : karame));
+		Order order = new Order(ticketLabel, lotOption, yasai, ninniku, abura, karame);
+		assertThrows(RuntimeException.class, () -> order.validate());
+	}
+
+	@ParameterizedTest
+	@DisplayName("注文内容にnullが含まれるときにバリデーションが失敗すること")
+	@CsvSource({
+	    ", なし, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "ラーメン,, 指定なし, 指定なし, 指定なし, 指定なし",
+	    "ラーメン, なし,, 指定なし, 指定なし, 指定なし",
+	    "ラーメン, なし, 指定なし,, 指定なし, 指定なし",
+	    "ラーメン, なし, 指定なし, 指定なし,, 指定なし",
+	    "ラーメン, なし, 指定なし, 指定なし, 指定なし,"
+	})
+	void nullValidation(String ticketLabel, String lotOption, String yasai, String ninniku, String abura, String karame) {
+		Order order = new Order(ticketLabel, lotOption, yasai, ninniku, abura, karame);
 		assertThrows(RuntimeException.class, () -> order.validate());
 	}
 
